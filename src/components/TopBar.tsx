c17dd8a9b5fa93c7c2eea1aca4dc0137b416ab5c
Mir Mohammad Asif Abdullah <mirmdasifabdullah@gmail.com>
2026-01-27 14:59:23 +0600

    check

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Smartphone} from 'lucide-react';
+import { Smartphone } from 'lucide-react';
 
 /**
  * TopBar Component
@@ -9,7 +9,7 @@ const TopBar = () => {
   return (
     <nav className="w-full bg-[#f3f2f3] text-grey-900 px-4 sm:px-6 py-2">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-2 md:gap-4">
-        
+
         {/* Left Side: Contact Info */}
         <div className="flex items-center gap-2">
           <Smartphone size={16} className="text-gray-900" />
@@ -21,16 +21,16 @@ const TopBar = () => {
 
         {/* Right Side: Social Media Icons */}
         <div className="flex items-center gap-4">
-          <a href="#" aria-label="Facebook" className="text-gray-900 hover:text-white transition-colors duration-200">
+          <a href="/" aria-label="Facebook" className="text-gray-900 hover:text-white transition-colors duration-200">
             <img src={`${process.env.PUBLIC_URL}/icons/fb.png`} alt="" className="social-icon" />
           </a>
-          <a href="#" aria-label="Twitter" className="text-gray-900 hover:text-white transition-colors duration-200">
+          <a href="/" aria-label="Twitter" className="text-gray-900 hover:text-white transition-colors duration-200">
             <img src={`${process.env.PUBLIC_URL}/icons/twitter.png`} alt="" className="social-icon" />
           </a>
-          <a href="#" aria-label="LinkedIn" className="text-gray-900 hover:text-white transition-colors duration-200">
+          <a href="/" aria-label="LinkedIn" className="text-gray-900 hover:text-white transition-colors duration-200">
             <img src={`${process.env.PUBLIC_URL}/icons/linkedin.png`} alt="" className="social-icon" />
           </a>
-          <a href="#" aria-label="Instagram" className="text-gray-900 hover:text-white transition-colors duration-200">
+          <a href="/" aria-label="Instagram" className="text-gray-900 hover:text-white transition-colors duration-200">
             <img src={`${process.env.PUBLIC_URL}/icons/insta.png`} alt="" className="social-icon" />
           </a>
         </div>
@@ -41,4 +41,4 @@ const TopBar = () => {
 };
 
 export default TopBar;
-export {}
+export { }
